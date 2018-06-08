ef7cd781012ae959bc00d4aaf35094085c4e8a36
Ronaldo C Leite <rleite1975@gmail.com>
2018-06-08 19:35:14 -0300

    Tratando erros 403

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable } from 'rxjs/Rx'; // IMPORTANTE: IMPORT ATUALIZADO
+import { StorageService } from '../services/storage.service';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
+    constructor(public storage: StorageService) {
+    }
+
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        console.log("Passou no interceptor");
+        //console.log("Passou no interceptor");
         return next.handle(req)
             .catch((error, caught) => {
 
@@ -21,8 +25,18 @@ export class ErrorInterceptor implements HttpInterceptor {
                 console.log("Erro detectado pelo interceptor:");
                 console.log(errorObj);
 
+                switch (errorObj.status) {
+                    case 403:
+                        this.handle403();
+                        break;
+                }
+
                 return Observable.throw(errorObj);
             }) as any;
+    }
+
+    handle403() {
+        this.storage.setLocalUser(null);
     }
 }
 
--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -29,8 +29,14 @@ export class ProfilePage {
           //buscar imagem
           this.getImageIfExists();
         },
-          error => { }
-        );
+          error => {
+            if (error.status == 403) {
+              this.navCtrl.setRoot('HomePage');
+            }
+          });
+    }
+    else {
+      this.navCtrl.setRoot('HomePage');
 
     }
   }
